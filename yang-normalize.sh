862d7cf399f4c6f3c0ef33a25a09df4b9353e58c
Brian Sipos <brian.sipos@jhuapl.edu>
2024-12-18 11:59:39 -0500

    Adding IDENTs for semantic type introspection and adding EDDs for object introspection

--- yang-normalize.sh
+++ yang-normalize.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+##
+## Copyright (c) 2011-2024 The Johns Hopkins University Applied Physics
+## Laboratory LLC.
+##
+## This file is part of the Delay-Tolerant Networking Management
+## Architecture (DTNMA) Tools package.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##     http://www.apache.org/licenses/LICENSE-2.0
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
 set -e
 
 USAGE="Usage: $0 [filename] {... filename}"
@@ -9,7 +26,7 @@ fi
 
 SELFDIR=$(readlink -f $(dirname "${BASH_SOURCE[0]}"))
 LINTOPTS="--ietf --lint-ensure-hyphenated-names"
-OUTOPTS="-f yang --yang-canonical"
+OUTOPTS="-t adm-add-enum -f yang --yang-canonical"
 NORMALIZE="ace_adm --path=${SELFDIR} ${LINTOPTS} ${OUTOPTS}"
 
 # Normalize a single YANG file
@@ -21,8 +38,8 @@ function normalize {
     shift
 
     if [ ! -f "${FILEPATH}" ]; then
-        echo "File is missing: ${FILEPATH}"
-        exit 1
+        echo "File is missing: ${FILEPATH}" >/dev/stderr
+        return 1
     fi
 
     # Canonicalize and normalize into ".out" file
@@ -30,8 +47,13 @@ function normalize {
     if [ "${EXT}" == "yang" ]; then
         ${NORMALIZE} "${FILEPATH}" >"${FILEPATH}.out"
     else
-        echo "Cannot handle file with extension: ${EXT}"
-        exit 1
+        echo "Cannot handle file with extension: ${EXT}" >/dev/stderr
+        return 2
+    fi
+    if [ ! -s "${FILEPATH}.out" ]; then
+        echo "Failed to format file ${FILEPATH}" >/dev/stderr
+        rm "${FILEPATH}.out"
+        return 3;
     fi
 
     if ! diff -q "${FILEPATH}.out" "${FILEPATH}" >/dev/null; then
